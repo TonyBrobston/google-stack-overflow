ae801ca996c4c36e56b14b6adb089a2f1fe8bf95
Tony Brobston <tbrobston@sourceallies.com>
2017-11-22 12:52:30 -0600

    WIP

--- TODO.md
+++
@@ -1,5 +0,0 @@
-# Google Stack Overflow
-
-## A Google form that defaults to `inurl: stackoverflow.com` so that you get only stackoverflow.com posts with the robust searching of google.com
-
-### Need to read these at some point: https://www.google.com/policies/terms/
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,25 @@
-function search() {
-    // alert(formatGoogleSearch(document.getElementById('searchText').value));
-    window.location.href = formatGoogleSearch(document.getElementById('searchText').value);
-}
+$.ajaxPrefilter( function (options) {
+  if (options.crossDomain && jQuery.support.cors) {
+    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+  }
+});
+
+// function search() {
+    // window.location.href = formatGoogleSearch(document.getElementById('searchText').value);
+// }
 
 function formatGoogleSearch(text) {
   return 'http://google.com/search?q=' + text.split(' ').join('+') + '+site%3Astackoverflow.com'
+}
+
+function search() {
+    $.get(
+        formatGoogleSearch(document.getElementById('searchText').value),
+        function (response) {
+            console.log("> ", response);
+            $("#viewer").html(response);
+        }
+    );
 }
