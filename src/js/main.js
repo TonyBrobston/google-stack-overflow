a6c3e2fb54d9073aab0f2e2c43ef6c0ad5379ca8
Tony Brobston <anthonybrobston@gmail.com>
2017-11-27 20:54:44 -0600

    Splitting out utils

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,6 @@ search = function() {
   window.location.href = formatGoogleSearch(searchText, ['stackoverflow']);
 }
 
-formatGoogleSearch = function(text, sites) {
-  return 'http://google.com/search?q=' + text + '+' + buildSiteList(sites);
+window.onload = function() {
+  document.getElementById('searchText').focus();
 }
-  
-buildSiteList = function(sites) {
-  for (var i = 0; i < sites.length; i++) {
-    sites[i] = 'site:' + sites[i] + '.com';
-  }
-  return '[' + sites.join('|') + ']';
-}
-
-//window.onload = function() {
-//  document.getElementById('searchText').focus();
-//}
---
+++ src/js/utils.js
@@ -0,0 +1,11 @@
+formatGoogleSearch = function(text, sites) {
+  return 'http://google.com/search?q=' + text + '+' + buildSiteList(sites);
+}
+  
+buildSiteList = function(sites) {
+  for (var i = 0; i < sites.length; i++) {
+    sites[i] = 'site:' + sites[i] + '.com';
+  }
+  return '[' + sites.join('|') + ']';
+}
+
