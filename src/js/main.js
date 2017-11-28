502495fb7d1726ed4e768fd17ae3ff4f978cae56
Tony Brobston <anthonybrobston@gmail.com>
2017-11-27 20:48:36 -0600

    Moving methods around

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,13 @@ search = function() {
   window.location.href = formatGoogleSearch(searchText, ['stackoverflow']);
 }
 
+formatGoogleSearch = function(text, sites) {
+  return 'http://google.com/search?q=' + text + '+' + buildSiteList(sites);
+}
+  
 buildSiteList = function(sites) {
   for (var i = 0; i < sites.length; i++) {
     sites[i] = 'site:' + sites[i] + '.com';
   }
   return '[' + sites.join('|') + ']';
 }
-
-formatGoogleSearch = function(text, sites) {
-  return 'http://google.com/search?q=' + text + '+' + buildSiteList(sites);
-}  
